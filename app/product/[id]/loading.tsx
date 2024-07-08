a5eb4e31ea4079003ad5c412adfb6849ab214f40
prakashcs44 <porkashbatarai@gmail.com>
2024-07-08 12:33:49 +0530

    fixed styling

--- app/product/[id]/loading.tsx
+++ app/product/[id]/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function loading() {
   return (
     <div className=" flex  items-center flex-col xl:flex-row gap-10 md:px-20 px-10 py-10 justify-center ">
-      <div className="w-[30%] h-96 skeleton border" />
+      <div className="md:w-5/12 w-3/4 h-96 skeleton border" />
       <div className="space-y-10  w-[60%]">
       <div className="w-[50%] h-10 skeleton" />
       <div className="w-[70%] h-8 skeleton" />
