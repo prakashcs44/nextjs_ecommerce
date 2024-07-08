7e8f583207ff1e84e4092a79c7676c1d32a0205a
prakashcs44 <porkashbatarai@gmail.com>
2024-07-08 12:32:21 +0530

    fixed some styling

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -16,8 +16,11 @@ async function page(props:any) {
 
 
   return (
-    <div className=' flex flex-col xl:flex-row gap-10 md:px-20 px-10 py-10'>
+    <div className='flex  flex-col items-center xl:flex-row gap-20 md:px-20 px-10 py-10'>
+       <div className='md:w-5/12 w-3/4'>
        <Carousel images={product?.images}/>
+       </div>
+      
       <div className='space-y-10'>
       <h1 className='font-bold text-3xl'>{product.title}</h1>
       <p>{product.description}</p>
